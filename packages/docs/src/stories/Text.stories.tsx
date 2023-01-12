6b26baa501a47ee68eaa466e4df5b8efaa44b3c7
Futhememe <kyoro_souza@hotmail.com>
2023-01-11 22:40:01 -0300

    ✨ create text story

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,42 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@futhedev/repo-dash'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children: 'Lorem ipsum',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      defaultValue: 'md',
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
